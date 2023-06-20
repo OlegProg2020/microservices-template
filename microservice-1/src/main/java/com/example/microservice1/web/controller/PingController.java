5932a563313d5bea1c64e3196d8c2d8d9c78a9ba
Oleg Minenko <justprog2020@yandex.ru>
2023-06-20 18:46:10 +0300

    add dependencies: spring-boot-starter-actuator, micrometer-tracing-bridge-brave; update: application.yaml (application.name, logging.pattern.level). New feature: distributed tracing

--- microservice-1/src/main/java/com/example/microservice1/web/controller/PingController.java
+++ microservice-1/src/main/java/com/example/microservice1/web/controller/PingController.java
@@ -6,13 +6,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-
 @RestController
-@RequestMapping(produces = APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 @Tag(name = "PingController")
 public class PingController {
