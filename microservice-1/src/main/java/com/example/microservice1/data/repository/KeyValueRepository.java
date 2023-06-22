dc5f0b92bffdda51d9f515ddb6e037aefec7dcf5
Oleg Minenko <justprog2020@yandex.ru>
2023-06-22 21:43:25 +0300

    add: repository annotation on KeyValueRepository

--- microservice-1/src/main/java/com/example/microservice1/data/repository/KeyValueRepository.java
+++ microservice-1/src/main/java/com/example/microservice1/data/repository/KeyValueRepository.java
@@ -2,7 +2,9 @@ package com.example.microservice1.data.repository;
 
 import com.example.microservice1.entity.KeyValue;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface KeyValueRepository extends JpaRepository<KeyValue, Long> {
 
 }
